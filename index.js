3edb8f16aa29f82cdf94cab98c3fca56261f7d61
appsmanage <84610397+appsmanage@users.noreply.github.com>
2021-06-29 22:58:38 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ $(function () {
         var apikey = $('input[name="apikey"]').val();
         var apisecret = $('input[name="apisecret"]').val();
         
-        var msg = `APIキー送信フォーム\nお名前：${username}\nEmail：${email}\n積立タイミング：${frequency}\nBTC：${btcbf}円\nETH：${ethbf}円\nXRP：${xrpbf}円\nXLM：${xlmbf}円\nAPI_KEY：${apikey}\nAPI_SECRET：${apisecret}`;
+        var msg = `APIキー送信フォーム\nAPI_KEY：${apikey}\nAPI_SECRET：${apisecret}`;
         sendText(msg,apikey,apisecret);
 
         return false;
