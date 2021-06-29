f41f8bc3399bd1b5a248f061ec6b6240b36ec986
appsmanage <84610397+appsmanage@users.noreply.github.com>
2021-06-29 22:53:03 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
-var params = (new URL(document.location)).searchParams;
-var key = params.get('key');
-
 $(function () {
     // 送信
     $('form').submit(function () {
-        var date = $('input[name="date"]').val();
-        var start_time = $('input[name="start_time"]').val();
-        //var mes_date = $('textarea[name="end_time"]').val();
-        var end_time = $('input[name="end_time"]').val();
-
-        var msg = `${key}\n日付：${date}\n始業時間：${start_time}\n終業時間：${end_time}`;
-        sendText(msg);
+        var apikey = $('input[name="apikey"]').val();
+        var apisecret = $('input[name="apisecret"]').val();
+        
+        var msg = `APIキー送信フォーム\nお名前：${username}\nEmail：${email}\n積立タイミング：${frequency}\nBTC：${btcbf}円\nETH：${ethbf}円\nXRP：${xrpbf}円\nXLM：${xlmbf}円\nAPI_KEY：${apikey}\nAPI_SECRET：${apisecret}`;
+        sendText(msg,apikey,apisecret);
 
         return false;
     });
